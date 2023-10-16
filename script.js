96666c97453a9f3f42ed63fad1c8f9ca9bea4676
Abhay Gupta <98942880+abhay272129@users.noreply.github.com>
2023-10-17 05:08:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count=0;
+	for (let index = 0; index < library.length; index++) {
+		if (library.readingStatus==true) {
+			count++;
+			
+		}
+		
+	}
+	return count;
 };
 
 // Do not change the code below
